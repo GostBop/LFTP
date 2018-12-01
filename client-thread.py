1b038f59af53c8e5403e107134e07312a7ca61c2
huangcm <826771695@qq.com>
2018-12-01 20:10:12 +0800

    实现一对三多线程

---
+++ client-thread.py
@@ -0,0 +1,210 @@
+# -*- coding=utf-8 -*-
+from __future__ import print_function
+from socket import *
+import pickle, random, threading, time, os, sys, Queue
+
+# -----------------------------------------------
+''' classes
+'''
+
+class c_pkt(object):
+  # client packet which has base(seq number) and data
+  def __init__(self, base, data):
+      self.base = base 
+      self.data = data
+
+class s_pkt(object):
+  # server packet which has ack and rwnd
+  def __init__(self, ack, rwnd):
+      self.ack = ack
+      self.rwnd = rwnd
+
+# -----------------------------------------------
+''' globals
+'''
+
+windows = Queue.Queue()
+
+def f():
+  a = 1
+seq_limit = 1000
+time_limit = 1
+time_count = 0
+timer = threading.Timer(time_limit, f)
+data_size = 10000
+packet_size = 60000
+
+ip = sys.argv[1]
+fileName = sys.argv[2]
+filePath = "D:\\" + fileName
+file_size = os.path.getsize(filePath)
+num_of_times = file_size / data_size
+
+base = 0 # (LstByteAcked + 1) % seq_limit
+nextseq = 0 # (LastByteSent + 1) % seq_limit
+rwnd = 100
+cwnd = 1
+ssthresh = 0
+
+count = 0
+start = 0
+end = 0
+
+lock = threading.Lock()
+
+tcp = socket(AF_INET, SOCK_STREAM)
+tcp.connect((ip, 9999))
+tcp.send(fileName)
+port = tcp.recv(1024)
+tcp.close()
+print("port: %d" % int(port))
+server_addr = (ip, int(port))
+# -----------------------------------------------
+''' functions
+'''
+
+def progress(percent , width = 50):
+	if percent >= 100:
+		percent = 100
+	show_str = ('[%%-%ds]' % width) % (int(width * percent / 100) * "#")
+	print('\r%s %d%%' % (show_str, percent), end = '')
+
+def resend():
+  '''
+    send all the packet in the windows, 
+    which have been sent but not acked yet
+  '''
+  
+  global timer, lock, time_count, cwnd, ssthresh
+  ssthresh = cwnd / 2
+  cwnd = 1
+  lock.acquire()
+  if end == 0:
+    win_size = windows.qsize()
+    if win_size == 0:
+      print("no resend")
+    for _ in range(0, win_size):
+      packet = windows.get()
+      client_socket.sendto(packet, server_addr)
+      p = pickle.loads(packet)
+      #print("resend %d" % int(p.base))
+      
+      windows.put(packet)
+  lock.release()
+  print("time_count %d" % time_count)
+  time_count = time_count + 1
+  timer.cancel()
+  timer = threading.Timer(time_limit + time_count, resend)
+  timer.start()
+
+def receive():
+  '''
+    receive from server, and if there is no response,
+    resend all the packet
+  '''
+  global base, timer, rwnd, count, start, lock, time_count, cwnd, ssthresh
+  while True:
+    if start == 0:
+      continue
+    if rwnd != 0:
+      timer = threading.Timer(time_limit, resend)
+      timer.start()
+    # if rwnd is 0, the transmit function will send(resend) packet
+    response, _ = client_socket.recvfrom(packet_size)
+    if response == "exit":
+      print(" server exit") 
+      end = 1
+      timer.cancel()
+      time.sleep(2)
+      break
+    else:
+
+      server_pkt = pickle.loads(response)
+      rwnd = int(server_pkt.rwnd)
+      if (int(server_pkt.ack) + 1) % seq_limit != base:
+        if cwnd >= ssthresh:
+          cwnd = cwnd + 1
+        else:
+          cwnd = cwnd * 2
+      print ("ack: %d, rwnd: %d, cwnd: %d" % (int(server_pkt.ack), int(server_pkt.rwnd), cwnd))
+      timer.cancel()
+      if int(server_pkt.ack) == base or int(server_pkt.ack) >= ((base + 1) % seq_limit):
+        base = (int(server_pkt.ack) + 1) % seq_limit
+        #print ("base: %d" % base)
+        
+
+        time_count = 0
+        lock.acquire()
+        while not windows.empty():
+          count = count + 1
+      
+          packet = windows.get()
+          p = pickle.loads(packet)
+          #print ("pop: %d" % int(p.base))
+          if ((p.base + 1) % seq_limit) >= base:
+            break
+        lock.release()
+        progress(100 * count / num_of_times)
+      
+
+def send_a_packet():
+  global f, data_size, nextseq, server_addr
+  data = f.read(data_size)
+  if data:
+    packet = pickle.dumps(c_pkt(nextseq, data))
+    client_socket.sendto(packet, server_addr)
+    windows.put(packet)
+    nextseq = (nextseq + 1) % seq_limit
+  else:
+    client_socket.sendto("exit " + str(nextseq), server_addr)
+
+def transmit():
+  '''
+    send new packet when there are still spaces,
+    and send a small packet when rwnd is 0
+  '''
+  global start, nextseq, base, seq_limit, rwnd, f, data_size, windows, lock, cwnd
+  rwnd_count = 0
+  while True:
+    # number of packets sent but not acked should smaller than rwnd
+    if (nextseq - base + seq_limit) % seq_limit < min(rwnd, cwnd):
+      data = f.read(data_size)
+      if data:
+        packet = pickle.dumps(c_pkt(nextseq, data))
+        client_socket.sendto(packet, server_addr)
+        start = 1
+        lock.acquire()
+        windows.put(packet)
+        lock.release()
+        #print("put: %d" % nextseq)
+        nextseq = (nextseq + 1) % seq_limit
+      else:
+        print(" send exit", end = '') 
+        client_socket.sendto("exit " + str(nextseq), server_addr)
+        windows.put(packet) # ?
+        break
+    if rwnd != 0:
+      rwnd_count = 0
+    # do sth when rwnd is 0
+    elif rwnd == 0:
+      #print ("rwnd == 0")
+      if rwnd_count % 10000 == 0:
+        #print("rwnd == 0")
+        client_socket.sendto("No Buffer", server_addr)
+      rwnd_count = (rwnd_count + 1) % 10000
+
+# -----------------------------------------------
+
+client_socket = socket(AF_INET, SOCK_DGRAM)
+f = open(filePath.decode('UTF-8'), 'rb')
+# global timer
+timer = threading.Timer(time_limit, resend)
+# receive from server
+t = threading.Thread(target = receive)
+t.start()
+# send packets
+transmit()
+
+t.join()
+f.close()
+client_socket.close()
